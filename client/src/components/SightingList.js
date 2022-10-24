4eb52967d550de66b356a6f89428ab9e0469db48
Collin Shawyer <collin@macbook-air.myfiosgateway.com>
2022-10-24 16:36:44 -0400

    initial error and sighting list

---
+++ client/src/components/SightingList.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { findAll } from "../services/sightings";
+
+
+function SightingList() {
+    const [sightings, setSightings] = useState([]);
+
+    const history = useHistory();
+
+    // useEffect(() => {
+    //     findAll()
+    //     .then(setSightings)
+    //     .catch(() => history.push("/error"))
+    // }, [history]);
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>User</th>
+                        <th>Bird</th>
+                        <th>City</th>
+                        <th>State</th>
+                        <th>DayTime</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sightings.map((s) => (
+                        <tr key={s.sightingId}>
+                            <td>{s.date}</td>
+                            <td>{s.sightingUserId}</td>
+                            <td>{s.sightingBirdId}</td>
+                            <td>{s.city}</td>
+                            <td>{s.state}</td>
+                            <td>{s.daytime}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default SightingList;
