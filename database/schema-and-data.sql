18016fc6256c3d8e7c099ebb80b77a9b4b36c4ab
Kris Salerno <ksalerno@dev-10.com>
2022-10-24 10:51:08 -0400

    initial bird schema

---
+++ database/schema-and-data.sql
@@ -0,0 +1,61 @@
+drop database if exists wing_span;
+create database wing_span;
+
+use wing_span;
+
+-- -----------------------------------------------------
+-- Table: trait
+-- -----------------------------------------------------
+create table trait (
+	trait_id int primary key auto_increment,
+    `name` varchar(50) not null
+);
+
+-- -----------------------------------------------------
+-- Table: bird
+-- -----------------------------------------------------
+create table bird  (
+	bird_id int primary key auto_increment,
+	common_name varchar(50) not null,
+    scientific_name varchar(50) not null,
+    image_url varchar(512) not null default 'https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmlyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+);
+  
+-- -----------------------------------------------------
+-- Table: bird_trait
+-- -----------------------------------------------------
+create table bird_trait (
+	bird_id int not null,
+    trait_id int not null,
+    constraint pk_bird_trait
+		primary key(bird_id, trait_id),
+	constraint fk_bird_trait_bird_id
+		foreign key(bird_id)
+        references bird(bird_id),
+	constraint fk_bird_trait_trait_id
+		foreign key (trait_id)
+        references trait(trait_id)
+);
+
+
+-- User related tables
+
+-- -----------------------------------------------------
+-- Table: sighting
+-- -----------------------------------------------------
+create table sighting (
+	user_id int not null,
+    bird_id int not null,
+    sighting_date date not null,
+    city varchar(50) not null,
+    state varchar(50) null,
+    daytime boolean null,
+    constraint pk_bird_trait
+		primary key(user_id, bird_id),
+    constraint fk_sighting_user_id
+		foreign key (user_id)
+		references `user`(user_id),
+	 constraint fk_sighting_bird_id
+		foreign key (bird_id)
+		references bird(bird_id)
+);
