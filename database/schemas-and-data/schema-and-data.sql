9bfcc427bd9a33c969afdd22c3749437c74c8d11
Kris Salerno <ksalerno@dev-10.com>
2022-10-24 14:19:51 -0400

    updated schema and data

--- database/schemas-and-data/schema-and-data.sql
+++ database/schemas-and-data/schema-and-data.sql
@@ -21,43 +21,25 @@ create table bird  (
 	bird_id int primary key auto_increment,
 	common_name varchar(50) not null,
     scientific_name varchar(50) not null,
-    image_url varchar(512) not null default 'https://images.unsplash.com/photo-1444464666168-49d633b86797?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmlyZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+    img_url varchar(512) not null
 );
   
--- -----------------------------------------------------
--- Table: bird_trait
--- -----------------------------------------------------
-create table bird_trait (
-	bird_id int not null,
-    trait_id int not null,
-    constraint pk_bird_trait
-		primary key(bird_id, trait_id),
-	constraint fk_bird_trait_bird_id
-		foreign key(bird_id)
-        references bird(bird_id),
-	constraint fk_bird_trait_trait_id
-		foreign key (trait_id)
-        references trait(trait_id)
-);
-
 -- -----------------------------------------------------
 -- Data: trait
 -- -----------------------------------------------------
 insert into trait (`name`) values
-	('Horizontal Posture'),
-    ('Vertical Posture'),
-    ('Alert'),
-    ('Skittish'),
-    ('Hopping'),
-    ('Walking'),
-    ('Flying'),
-    ('Nest Buidling'),
-    ('Nesting'),
-    ('Dabbling'),
-    ('Diving'),
-    ('Floating'),
+	('Perching'),
+	('Nesting'),
     ('Wings Open'),
     ('Wings Down'),
+	('Horizontal Posture'),
+    ('Vertical Posture'),
+    ('Hopping'),
+    ('Walking'),
+    ('Diving into Water'),
+    ('Floating On Water'),
+	('Dabbling'),
+    ('Flying'),
     ('Slow Wing Flaps'),
     ('Fast Wing Flaps'),
     ('Swooping Flight'),
@@ -65,21 +47,104 @@ insert into trait (`name`) values
     ('Circular Flight Path'),
     ('Gliding'),
     ('Hovering'),
-    ('Feather Grooming'),
+	('Flocking'),
+    ('Migrating'),
+    ('Grooming'),
     ('Molting'),
-    ('Ground Feeding'),
-    ('Canopy Feeding'),
-    ('Bark Feeding'),
-    ('Aerial Feeding'),
-    ('Perch Hunting'),
-    ('Aerial Hunting'),
-    ('Aquatic Hunting'),
+    ('Feeding'),
+    ('Hunting'),
     ('Singing'),
-    ('Screaming/Mobbing'),
     ('Fighting'),
     ('Courting'),
     ('Mating'),
     ('Fledgling'),
-    ('Spotted Plumage'),
-    ('Flocking'),
-    ('Migrating');
+    ('Adult');
+    
+-- -----------------------------------------------------
+-- Data: bird
+-- -----------------------------------------------------
+-- common_name varchar(50) not null,
+--     scientific_name varchar(50) not null,
+--     img_url varchar(512) not null
+insert into bird (common_name, scientific_name, img_url) values
+	('Woodpecker', 'Picidae', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Woodpecker_20040529_151837_1c_cropped.JPG/330px-Woodpecker_20040529_151837_1c_cropped.JPG'),
+    ('Pigeon', 'Columbidae', 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Treron_vernans_male_-_Kent_Ridge_Park.jpg/330px-Treron_vernans_male_-_Kent_Ridge_Park.jpg'),
+    ('Peacock', 'Pavo Cristatus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Peacock_Plumage.jpg/330px-Peacock_Plumage.jpg'),
+    ('Turkey Vulture', 'Cathartes Aura', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Urubu_a_tete_rouge_-_Turkey_Vulture.jpg/330px-Urubu_a_tete_rouge_-_Turkey_Vulture.jpg'),
+    ('Tree Swallow', 'Tachycineta Bicolor', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Tree_swallow_in_JBWR_%2825579%29.jpg/330px-Tree_swallow_in_JBWR_%2825579%29.jpg'),
+    ('Banded Martin', 'Neophedina Cincta', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Banded_martin%2C_or_banded_sand_martin%2C_Riparia_cincta%2C_at_Rietvlei_Nature_Reserve%2C_Gauteng%2C_South_Africa_%2831326868871%29.jpg/330px-Banded_martin%2C_or_banded_sand_martin%2C_Riparia_cincta%2C_at_Rietvlei_Nature_Reserve%2C_Gauteng%2C_South_Africa_%2831326868871%29.jpg'),
+    ('Common Gull', 'Larus Canus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Larus_canus_Common_Gull_in_Norway.jpg/330px-Larus_canus_Common_Gull_in_Norway.jpg'),
+    ('Northern Bobwhite', 'Colinus Virginianus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Virginiawachtel_2007-06-16_065.jpg/330px-Virginiawachtel_2007-06-16_065.jpg'),
+    ('Mallard Duck', 'Anas Platyrhynchos', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Anas_platyrhynchos_male_female_quadrat.jpg/330px-Anas_platyrhynchos_male_female_quadrat.jpg'),
+    ('Great White Pelican', 'Pelecanus Onocrotalus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Great_white_pelican_%28Pelecanus_onocrotalus%29.jpg/330px-Great_white_pelican_%28Pelecanus_onocrotalus%29.jpg'),
+    ('Common Magpie', 'Pica Pica', 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Pica_pica_-_Compans_Caffarelli_-_2012-03-16.jpg/330px-Pica_pica_-_Compans_Caffarelli_-_2012-03-16.jpg'),
+    ('Cockatiel', 'Nymphicus Hollandicus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Cockatielmale.jpg/330px-Cockatielmale.jpg'),
+    ('Wild Turkey', 'Meleagris Gallopavo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Gall-dindi.jpg/330px-Gall-dindi.jpg'),
+    ('Whooping Crane', 'Grus Americana', 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Grus_americana_Sasata.jpg/330px-Grus_americana_Sasata.jpg'),
+    ('Common Kingfisher', 'Alcedo Atthis', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg/330px-%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg'),
+    ("Allen's Hummingbird", 'Selasphorus Sasin', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Allen%27s_Hummingbird_Guarding_Flower_Patch.jpg/330px-Allen%27s_Hummingbird_Guarding_Flower_Patch.jpg'),
+    ('House Sparrow', 'Passer Domesticus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Passer_domesticus_male_%2815%29.jpg/330px-Passer_domesticus_male_%2815%29.jpg'),
+    ('Great Cormorant', 'Phalacrocorax Carbo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Phalacrocorax_carbo_Vic.jpg/330px-Phalacrocorax_carbo_Vic.jpg');
+    
+-- Ostrich
+-- Crow
+-- Raven
+-- Dove
+-- Hen
+-- Nightingale
+-- Eagle
+-- Swan
+-- Penguin
+-- Flamingo
+-- Goose
+-- Cuckoo
+-- Owl
+-- Hawk
+-- Partridge
+-- Goldfinch
+-- Robin
+-- Finch
+-- Frigatebird
+-- Sandpiper
+-- Stork
+-- Ibis
+-- Hornbill
+-- Bulbul
+-- Skylark
+-- Canary
+-- Wagtail
+-- Starling
+-- Macaw
+-- Cockatoo
+-- Heron
+-- Toucan
+-- Jay
+-- Mynah
+-- Cardinal
+-- Chickadee
+-- Junco
+-- Bluebird
+-- Swift
+-- Gull
+-- Lovebird
+-- Spoonbill
+-- Kiwi
+-- Avocet
+-- Wren
+-- Mockingbird
+-- Pheasant
+-- Hoopoe
+-- Kite
+-- Peahen
+-- Falcon
+-- Mallard
+-- Bald eagle
+-- Tern
+-- Night hawk
+-- Crossbill
+-- Lapwing
+-- Puffin
+-- Koyal
+-- Bullfinch
+-- Emu
+-- Condor
--- database/schemas-and-data/user-schema-data.sql
+++ database/schemas-and-data/user-schema-data.sql
@@ -4,6 +4,7 @@ drop table if exists app_user_role;
 drop table if exists app_role;
 drop table if exists app_user;
 drop table if exists sighting;
+drop table if exists sighting_trait;
 drop table if exists user_badge;
 drop table if exists badge;
 drop table if exists user_avatar;
@@ -50,20 +51,35 @@ create table app_user_role (
 -- Table: sighting (user related table)
 -- -----------------------------------------------------
 create table sighting (
-	app_user_id int not null,
+	sighting_id int primary key auto_increment,
+	user_id int not null,
     bird_id int not null,
     sighting_date date not null,
     city varchar(50) not null,
     state varchar(50) null,
     daytime boolean null,
-    constraint pk_bird_trait
-		primary key(app_user_id, bird_id),
     constraint fk_sighting_user_id
 		foreign key (app_user_id)
 		references app_user(app_user_id),
 	 constraint fk_sighting_bird_id
 		foreign key (bird_id)
 		references bird(bird_id)
+);
+
+-- -----------------------------------------------------
+-- Table: sighting_trait
+-- -----------------------------------------------------
+create table sighting_trait (
+	sighting_id int not null,
+    trait_id int not null,
+    constraint pk_sighting_trait
+		primary key(sighting_id, trait_id),
+	constraint fk_sighting_trait_sighting_id
+		foreign key(sighting_id)
+        references sighting(sighting_id),
+	constraint fk_sighting_trait_trait_id
+		foreign key (trait_id)
+        references trait(trait_id)
 );
 
 -- -----------------------------------------------------
@@ -128,9 +144,9 @@ insert into app_role (`name`) values
 -- Data: app_user
 -- -----------------------------------------------------
 -- Initial data to get started, passwords are set to "P@ssw0rd!" for now
-insert into app_user (username, password_hash) values
-    ('admin@admin.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa'),
-    ('user@user.com', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa');
+insert into app_user (username, password_hash, email, user_first_name, user_last_name) values
+    ('admin1', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 'admin@admin.com', 'Test', 'McTesterson'),
+    ('user1', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 'user@user.com', 'User', 'McUser');
 
 -- -----------------------------------------------------
 -- Data: app_user_role
@@ -141,29 +157,21 @@ insert into app_user_role (app_user_id, app_role_id) values
     (2, 1);
     
 -- -----------------------------------------------------
--- Data: sightings
--- -----------------------------------------------------
-    
--- -----------------------------------------------------
 -- Data: badge
 -- -----------------------------------------------------
-insert into badge (badge_id, badge_name, badge_description) values
-	(1, '5 Sightings', "You've made 5 sightings!"),
-    (2, '10 Sightings', "You've made 10 sightings!"),
-    (3, '20 Sightings', "You've made 20 sightings!"),
-    (4, '50 Sightings', "You've made 50 sightings!");
-
--- -----------------------------------------------------
--- Data: user_badge
--- -----------------------------------------------------
+insert into badge (badge_name, badge_description) values
+	('5 Sightings', "You've made 5 sightings!"),
+    ('10 Sightings', "You've made 10 sightings!"),
+    ('20 Sightings', "You've made 20 sightings!"),
+    ('50 Sightings', "You've made 50 sightings!");
 
 -- -----------------------------------------------------
 -- Data: avatar
 -- -----------------------------------------------------
-insert into avatar (avatar_id, avatar_img_url, avatar_description) values
-	(1,'https://cdn.pixabay.com/photo/2017/05/25/21/26/bird-feeder-2344414__480.jpg', "Birdhouse"),
-    (2, 'https://cdn.pixabay.com/photo/2022/09/29/08/39/european-robin-7486889__480.jpg', "Birdbath"),
-    (3, 'https://cdn.pixabay.com/photo/2015/10/19/10/32/binoculars-995779__480.jpg', "Binoculars");
+insert into avatar (avatar_img_url, avatar_description) values
+	('https://cdn.pixabay.com/photo/2017/05/25/21/26/bird-feeder-2344414__480.jpg', "Birdhouse"),
+    ('https://cdn.pixabay.com/photo/2022/09/29/08/39/european-robin-7486889__480.jpg', "Birdbath"),
+    ('https://cdn.pixabay.com/photo/2015/10/19/10/32/binoculars-995779__480.jpg', "Binoculars");
     
 -- -----------------------------------------------------
 -- Data: user_avatar
