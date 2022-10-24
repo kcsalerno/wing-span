3551084491bab0aa0a288e010147cb9a47990a46
graciakaneza <gkaneza@asu.edu>
2022-10-24 14:26:30 -0400

    models updates

--- server/src/main/java/learn/wing_span/models/Avatar.java
+++ server/src/main/java/learn/wing_span/models/Avatar.java
@@ -1,4 +1,31 @@
 package learn.wing_span.models;
 
 public class Avatar {
+    private int avatarId;
+    private String imageUrl;
+    private String avatarDescription;
+
+    public int getAvatarId() {
+        return avatarId;
+    }
+
+    public void setAvatarId(int avatarId) {
+        this.avatarId = avatarId;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getAvatarDescription() {
+        return avatarDescription;
+    }
+
+    public void setAvatarDescription(String avatarDescription) {
+        this.avatarDescription = avatarDescription;
+    }
 }
--- server/src/main/java/learn/wing_span/models/Bird.java
+++ server/src/main/java/learn/wing_span/models/Bird.java
@@ -1,4 +1,51 @@
 package learn.wing_span.models;
 
 public class Bird {
+    private int birdId;
+    private String commonName;
+    private String scientificName;
+    private String imageUrl;
+
+    private Bird(){
+
+    }
+
+    public Bird(int birdId, String commonName, String scientificName, String imageUrl) {
+        this.birdId = birdId;
+        this.commonName = commonName;
+        this.scientificName = scientificName;
+        this.imageUrl = imageUrl;
+    }
+
+    public int getBirdId() {
+        return birdId;
+    }
+
+    public void setBirdId(int birdId) {
+        this.birdId = birdId;
+    }
+
+    public String getCommonName() {
+        return commonName;
+    }
+
+    public void setCommonName(String commonName) {
+        this.commonName = commonName;
+    }
+
+    public String getScientificName() {
+        return scientificName;
+    }
+
+    public void setScientificName(String scientificName) {
+        this.scientificName = scientificName;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
