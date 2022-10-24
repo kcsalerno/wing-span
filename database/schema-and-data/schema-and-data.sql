73521d81da51b9f7facfb65fd4a4f6fe27b51cac
Kris Salerno <ksalerno@dev-10.com>
2022-10-24 11:24:51 -0400

    initial schema complete

--- database/schema-and-data/schema-and-data.sql
+++ database/schema-and-data/schema-and-data.sql
@@ -1,6 +1,3 @@
--- -----------------------------------------------------
--- Note: This file must be run first.
--- -----------------------------------------------------
 drop database if exists wing_span;
 create database wing_span;
 
@@ -38,4 +35,27 @@ create table bird_trait (
 	constraint fk_bird_trait_trait_id
 		foreign key (trait_id)
         references trait(trait_id)
+);
+
+
+-- User related tables
+
+-- -----------------------------------------------------
+-- Table: sighting
+-- -----------------------------------------------------
+create table sighting (
+	user_id int not null,
+    bird_id int not null,
+    sighting_date date not null,
+    city varchar(50) not null,
+    state varchar(50) null,
+    daytime boolean null,
+    constraint pk_bird_trait
+		primary key(user_id, bird_id),
+    constraint fk_sighting_user_id
+		foreign key (user_id)
+		references `user`(user_id),
+	 constraint fk_sighting_bird_id
+		foreign key (bird_id)
+		references bird(bird_id)
 );
---
+++ database/user-schema-data.sql
@@ -0,0 +1,120 @@
+drop database if exists wing_span;
+create database wing_span;
+
+use wing_span;
+
+drop table if exists app_user_role;
+drop table if exists app_role;
+drop table if exists app_user;
+drop table if exists sighting;
+drop table if exists user_badge;
+drop table if exists bade;
+drop table if exists user_avatar;
+drop table if exists avatar;
+
+-- -----------------------------------------------------
+-- Table: app_user
+-- -----------------------------------------------------
+create table app_user (
+	app_user_id int primary key auto_increment,
+    username varchar(50) not null unique,
+    password_hash varchar(2048) not null,
+    enabled boolean not null default(true),
+    email varchar(255) not null,
+    user_first_name varchar(50) null,
+    user_last_name varchar(50) null
+);
+
+-- -----------------------------------------------------
+-- Table: user_role
+-- -----------------------------------------------------
+create table app_role (
+	app_role_id int primary key auto_increment,
+    `name` varchar(50) not null unique
+);
+
+-- -----------------------------------------------------
+-- Table: app_user_role
+-- -----------------------------------------------------
+create table app_user_role (
+	app_user_id int not null,
+    app_role_id int not null,
+    constraint pk_app_user_role
+		primary key (app_user_id, app_role_id),
+	constraint fk_app_user_role_user_id
+		foreign key (app_user_id)
+        references app_user(app_user_id),
+	constraint fk_app_user_role_role_id
+		foreign key	(app_role_id)
+        references app_role(app_role_id)
+);
+
+-- -----------------------------------------------------
+-- Table: sighting (user related table)
+-- -----------------------------------------------------
+create table sighting (
+	app_user_id int not null,
+    bird_id int not null,
+    sighting_date date not null,
+    city varchar(50) not null,
+    state varchar(50) null,
+    daytime boolean null,
+    constraint pk_bird_trait
+		primary key(app_user_id, bird_id),
+    constraint fk_sighting_user_id
+		foreign key (app_user_id)
+		references app_user(user_id),
+	 constraint fk_sighting_bird_id
+		foreign key (bird_id)
+		references bird(bird_id)
+);
+
+-- -----------------------------------------------------
+-- Table: badge (user related table)
+-- -----------------------------------------------------
+create table badge (
+	badge_id int primary key auto_increment,
+    badge_name varchar(50) not null,
+    badge_description varchar(50) not null
+);
+
+-- -----------------------------------------------------
+-- Table: user_badge
+-- -----------------------------------------------------
+create table user_badge (
+	app_user_id int not null,
+    badge_id int not null,
+    constraint pk_user_badge
+		primary key (app_user_id, badge_id),
+	constraint fk_user_badge_user_id
+		foreign key (app_user_id)
+		references app_user(user_id),
+	 constraint fk_user_badge_badge_id
+		foreign key (badge_id)
+		references badge(badge_id)
+);
+
+-- -----------------------------------------------------
+-- Table: avatar
+-- -----------------------------------------------------
+create table avatar (
+	avatar_id int primary key auto_increment,
+	avatar_imd_url varchar(512) not null default 'https://media.istockphoto.com/photos/friends-using-binoculars-in-a-forest-picture-id1356557792?b=1&k=20&m=1356557792&s=170667a&w=0&h=5qzcB3V-uAA9hqIzfZqV4z2pUJ-bopoCUFqzwpE6v6o=',
+	avatar_description varchar (100) null
+);
+  
+-- -----------------------------------------------------
+-- Table: user_avatar
+-- -----------------------------------------------------
+create table user_avatar (
+	app_user_id int not null,
+	avatar_id int not null,
+	constraint pk_user_avatar
+		primary key (app_user_id, avatar_id),
+	constraint fk_user_avatar_user_id
+		foreign key (app_user_id)
+		references app_user(user_id),
+	constraint fk_user_avatar_avatar_id
+		foreign key (avatar_id)
+		references avatar(avatar_id)   
+);
