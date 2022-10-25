a40de96ab7a13ff35423d43da1b6e0725e274de3
graciakaneza <gkaneza@asu.edu>
2022-10-25 10:01:34 -0400

    data layer in progress

--- server/src/main/java/learn/wing_span/data/AvatarJdbcTemplateRepository.java
+++ server/src/main/java/learn/wing_span/data/AvatarJdbcTemplateRepository.java
@@ -1,4 +1,66 @@
 package learn.wing_span.data;
 
-public class AvatarJdbcTemplateRepository {
+import learn.wing_span.data.mappers.AvatarMapper;
+import learn.wing_span.models.Avatar;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+public class AvatarJdbcTemplateRepository implements AvatarRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public AvatarJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Avatar findById(int avatarId) {
+        final String sql = "select avatar_id, avatar_img_url, avatar_description "
+                + "from avatar "
+                + "where avatar_id = ?;";
+        return jdbcTemplate.query(sql, new AvatarMapper(), avatarId).stream()
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public Avatar add(Avatar avatar) {
+        final String sql = "insert into avatar (avatar_img_url, avatar_description)"
+                + "values (?,?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, avatar.getImageUrl());
+            ps.setString(2, avatar.getAvatarDescription());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        avatar.setAvatarId(keyHolder.getKey().intValue());
+        return avatar;
+    }
+
+    @Override
+    public boolean update(Avatar avatar) {
+        final String sql = "update avatar set "
+                + "avatar_img_url = ?, "
+                + "avatar_description = ? "
+                + "where avatar_id = ?;";
+        return jdbcTemplate.update(sql,
+                avatar.getImageUrl(),
+                avatar.getAvatarDescription(),
+                avatar.getAvatarId()) > 0;
+    }
+
+    @Override
+    public boolean deleteById(int avatarId) {
+        return jdbcTemplate.update("delete from avatar where avatar_id = ?", avatarId) > 0;
+    }
 }
--- server/src/main/java/learn/wing_span/data/BirdJdbcTemplateRepository.java
+++ server/src/main/java/learn/wing_span/data/BirdJdbcTemplateRepository.java
@@ -1,4 +1,45 @@
 package learn.wing_span.data;
 
-public class BirdJdbcTemplateRepository {
+import learn.wing_span.models.Bird;
+import learn.wing_span.models.Sighting;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class BirdJdbcTemplateRepository implements BirdRepository{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public BirdJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Bird> findAll() {
+        return null;
+    }
+
+    @Override
+    public Bird findById(int BirdId) {
+        return null;
+    }
+
+    @Override
+    public Bird add(Bird bird) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Bird bird) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(int birdId) {
+        return false;
+    }
+
+    private void addSightings(Sighting sighting) {
+        String sql = "select ";
+    }
 }
