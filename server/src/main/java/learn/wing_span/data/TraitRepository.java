b7e7f2c423ae49aeefe9bcd673c7cfaaec06c32f
graciakaneza <gkaneza@asu.edu>
2022-10-25 15:34:15 -0400

    data layer

--- server/src/main/java/learn/wing_span/data/TraitRepository.java
+++ server/src/main/java/learn/wing_span/data/TraitRepository.java
@@ -2,7 +2,10 @@ package learn.wing_span.data;
 
 import learn.wing_span.models.Trait;
 
+import java.util.List;
+
 public interface TraitRepository {
+    List<Trait> findAll();
     Trait findById(int traitId);
     Trait add(Trait trait);
     boolean update(Trait trait);
