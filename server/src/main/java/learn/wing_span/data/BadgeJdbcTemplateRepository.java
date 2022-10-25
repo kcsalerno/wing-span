9d2b4d335437ef67bd0506e20129e168ec7d2e92
graciakaneza <gkaneza@asu.edu>
2022-10-25 17:01:07 -0400

    data layer

--- server/src/main/java/learn/wing_span/data/BadgeJdbcTemplateRepository.java
+++ server/src/main/java/learn/wing_span/data/BadgeJdbcTemplateRepository.java
@@ -20,7 +20,7 @@ public class BadgeJdbcTemplateRepository implements BadgeRepository{
 
     @Override
     public Badge findById(int badgeId) {
-        final String sql = "select badge_id, badge_name, badge_description "
+        final String sql = "select badge_id, badge_name, badge_description, badge_img_url "
                 + "from badge "
                 + "where badge_id = ?;";
         return jdbcTemplate.query(sql, new BadgeMapper(), badgeId).stream()
@@ -30,14 +30,15 @@ public class BadgeJdbcTemplateRepository implements BadgeRepository{
 
     @Override
     public Badge add(Badge badge) {
-        final String sql = "insert into badge (badge_name, badge_description)"
-                + "values (?,?);";
+        final String sql = "insert into badge (badge_name, badge_description, badge_img_url)"
+                + "values (?,?,?);";
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         int rowsAffected = jdbcTemplate.update(connection -> {
             PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, badge.getBadgeName());
             ps.setString(2, badge.getBadgeDescription());
+            ps.setString(3, badge.getBadgeImgUrl());
             return ps;
         }, keyHolder);
 
@@ -53,12 +54,15 @@ public class BadgeJdbcTemplateRepository implements BadgeRepository{
     public boolean update(Badge badge) {
         final String sql = "update badge set "
                 + "badge_name = ?, "
-                + "badge_description = ? "
+                + "badge_description = ?, "
+                + "badge_img_url = ? "
                 + "where badge_id = ?;";
-        return jdbcTemplate.update(sql,
+        int rowsAffected = jdbcTemplate.update(sql,
                 badge.getBadgeName(),
                 badge.getBadgeDescription(),
-                badge.getBadgeId()) > 0;
+                badge.getBadgeId());
+
+        return rowsAffected > 0;
     }
 
     @Override
