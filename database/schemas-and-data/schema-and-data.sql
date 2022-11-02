e697ccf6e39df6fc09a751c242b4c454cd330384
Collin Shawyer <collin@macbook-air.myfiosgateway.com>
2022-11-02 13:08:53 -0400

    added beautiful duck

--- database/schemas-and-data/schema-and-data.sql
+++ database/schemas-and-data/schema-and-data.sql
@@ -56,4 +56,5 @@ insert into bird (common_name, scientific_name, img_url) values
     ('Common Kingfisher', 'Alcedo Atthis', 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg/330px-%E2%99%82_Common_Kingfisher_%28Alcedo_atthis%29_Photograph_By_Shantanu_Kuveskar%2C_Mangaon%2C_Maharashtra%2C_India.jpg'),
     ("Allen's Hummingbird", 'Selasphorus Sasin', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Allen%27s_Hummingbird_Guarding_Flower_Patch.jpg/330px-Allen%27s_Hummingbird_Guarding_Flower_Patch.jpg'),
     ('House Sparrow', 'Passer Domesticus', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Passer_domesticus_male_%2815%29.jpg/330px-Passer_domesticus_male_%2815%29.jpg'),
-    ('Great Cormorant', 'Phalacrocorax Carbo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Phalacrocorax_carbo_Vic.jpg/330px-Phalacrocorax_carbo_Vic.jpg');
+    ('Great Cormorant', 'Phalacrocorax Carbo', 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/68/Phalacrocorax_carbo_Vic.jpg/330px-Phalacrocorax_carbo_Vic.jpg'),
+    ('Mandarin Duck', 'Aix Galericulata', 'https://justbirding.com/wp-content/uploads/2018/11/mandarin-duck-Aix-galericulata.jpg');
