0a93f536a1d88f98411ead4c05c58051d5d6e41a
Collin Shawyer <collin@macbook-air.myfiosgateway.com>
2022-11-02 23:18:12 -0400

    fixed typos

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -3,10 +3,10 @@ function About() {
         <div className="about">
             <div className="text">
                 <h2>About WingSpan</h2><br/>
-                <p>WingSpan is grassroots, open source bird watching platform dedicated to making your wildlife experiences more enjoyable and easier to track.</p>
-                <p>Track your sightings, record what you saw, and add new birds to our ever growing database all the while earning badges for your contributions. A speical shout our friends at <a href="https://thenounproject.com/" target="_blank" rel="noreferrer noopener">The Noun Project</a> for developing those super cool badges.</p>
+                <p>WingSpan is a grassroots, open source bird watching platform dedicated to making your wildlife experiences more enjoyable and easier to track.</p>
+                <p>Document your sightings, record what you saw, and add new birds to our ever growing database all the while earning badges for your contributions. A special shout out to our friends at <a href="https://thenounproject.com/" target="_blank" rel="noreferrer noopener">The Noun Project</a> for developing those super cool badges.</p>
                 <p>Help us build a community fellow avian enthusiasts can call home!</p><br/>
-                <i>We are still in beta so please come back frequently for new features and further developments.</i>
+                <i>We are still in beta so please visit us often for new features and further developments.</i>
             </div>
             <div className="image-container">
                 <img src="https://justbirding.com/wp-content/uploads/2018/11/mandarin-duck-Aix-galericulata.jpg" alt="a cute mandarin duck"/>
